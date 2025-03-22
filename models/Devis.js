1cddca5245f08c1f54275132b6ce25b3be5faa49
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2025-03-22 17:15:39 +0300

    feat:models-v3

--- models/Devis.js
+++ models/Devis.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const DevisSchema = new mongoose.Schema({
     utilisateur: { type: mongoose.Schema.Types.ObjectId, ref: 'Utilisateur', required: true },
-    typeVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'TypeVehicule', required: true },
+    categorieVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'CategorieVehicule', required: true },
+    modeleVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'ModeleVehicule', required: true },
+    marqueVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'MarqueVehicule', required: true },
     totalPrix: { type: Number, required: true },
     nombreMecanicien: { type: Number, required: true },
     totalHeure: { type: String, required: true } // Stocké comme string HH:MM:SS
 }, { timestamps: true });
 
-module.exports = mongoose.model('Devis', DevisSchema);
+const Devis = mongoose.model('Devis', DevisSchema);
--- models/Pieces.js
+++ models/Pieces.js
@@ -3,8 +3,9 @@ const PiecesSchema = new mongoose.Schema({
     nomPiece: { type: String, required: true },
     reference: { type: String, required: true },
     categorieVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'CategorieVehicule', required: true },
-    modeleVehicule: { type: String, required: true },
+    modeleVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'ModeleVehicule', required: true },
+    marqueVehicule: { type: mongoose.Schema.Types.ObjectId, ref: 'MarqueVehicule', required: true },
     prix: { type: Number, required: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Pieces', PiecesSchema);
+const Pieces = mongoose.model('Pieces', PiecesSchema);
