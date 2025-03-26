c9f7016f709bab015672c19036a3a54878822190
notiaa <120126861+notiaa@users.noreply.github.com>
2025-03-26 14:56:51 +0300

    feature:integration-donne

--- models/Majoration.js
+++ models/Majoration.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const MajorationSchema = new mongoose.Schema({
-  _idtache: { type: mongoose.Schema.Types.ObjectId, ref: 'Tache',required: true },
   pourcentage: { type: Number, required: true }
 }, { timestamps: true });
 
