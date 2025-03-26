df4997b908b0ca2023efad818892789f56e8ff12
notiaa <120126861+notiaa@users.noreply.github.com>
2025-03-26 18:05:18 +0300

    fix:pieces-services

--- routes/Services.js
+++ routes/Services.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Service = require('../models/Service');
 const upload = require("../middlewares/upload"); 
+const Tache = require("../models/Tache");
 
 // CRUD pour Service
 
@@ -68,6 +69,38 @@ router.delete('/:id', async (req, res) => {
     res.json(deletedItem);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+
+
+
+// Route pour récupérer les services avec leurs tâches associées
+router.get("/services-taches", async (req, res) => {
+  try {
+    // Récupérer tous les services
+    const services = await Service.find();
+
+    // Pour chaque service, récupérer les tâches associées
+    const servicesWithTaches = await Promise.all(
+      services.map(async (service) => {
+        const taches = await Tache.find({ _idService: service._id });
+        return {
+          ...service.toObject(),
+          taches: taches.map((t) => ({
+            id: t._id,
+            name: t.nom,
+            description: t.description,
+            prix: t.prix,
+            selected: false, // Par défaut non sélectionné
+          })),
+        };
+      })
+    );
+
+    res.json(servicesWithTaches);
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur", error });
   }
 });
 
